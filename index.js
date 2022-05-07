fc671fe7f725416b2b0d4e66b367d7a86fc33400
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-07 13:45:03 +0600

    all items get req create and respons with API and get from DataBase

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -14,12 +15,24 @@ app.use(express.json()); // body theke data parse korte lage
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yeb55.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('data base connected');
-    client.close();
-});
+async function run(){
+    try{
+        await client.connect();
+        const itemsCollection = client.db("allItems").collection("itmes")
+
+        app.get('/items', async(req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+    }finally{
+        // await client.close()
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World! this is zaman')
